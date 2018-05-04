018cb441e570446c8ba4a01f038e9d35f38802c2
zsp <zsp@bcd8e591-16ba-7b4b-906a-4d9d008add67>
2018-05-04 05:48:55 +0000

    git-svn-id: https://192.168.199.202/svn/zsp@10 bcd8e591-16ba-7b4b-906a-4d9d008add67

---
+++ AuctionSystem/UI.Logic/Model/MemberInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Logic.Model
+{
+	/// <summary>
+	/// 会员信息表
+	/// </summary>
+	public class MemberInfo
+	{
+		/// <summary>
+		/// 会员信息ID
+		/// </summary>
+		public int Id { get; set; }
+		/// <summary>
+		/// 会员ID，外键
+		/// </summary>
+		public int MemberId { get; set; }
+		/// <summary>
+		/// 真实姓名
+		/// </summary>
+		public string RealName { get; set; }
+		/// <summary>
+		/// 出生日期
+		/// </summary>
+		public DateTime Birthdate { get; set; }
+		/// <summary>
+		/// 邮箱
+		/// </summary>
+		public string Email { get; set; }
+		/// <summary>
+		/// 收货地址
+		/// </summary>
+		public string ShippingAddress { get; set; }
+	}
+}
---
+++ AuctionSystem/UI.Logic/Model/Order.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Logic.Model
+{
+	/// <summary>
+	/// 订单表
+	/// </summary>
+	public class Order
+	{
+		/// <summary>
+		/// 订单ID
+		/// </summary>
+		public int Id { get; set; }
+		/// <summary>
+		/// 商品ID，外键
+		/// </summary>
+		public int ProductId { get; set; }
+		/// <summary>
+		/// 会员ID，外键
+		/// </summary>
+		public int MemberId { get; set; }
+		/// <summary>
+		/// 快递单号
+		/// </summary>
+		public string ExpressNumber { get; set; }
+		/// <summary>
+		/// 配送方式
+		/// </summary>
+		public string DeliveryMethod { get; set; }
+		/// <summary>
+		/// 备注
+		/// </summary>
+		public string Remark { get; set; }
+		/// <summary>
+		/// 支付方式
+		/// </summary>
+		public string PayMethod { get; set; }
+		/// <summary>
+		/// 支付时间
+		/// </summary>
+		public DateTime? PayTime { get; set; }
+		/// <summary>
+		/// 提交时间
+		/// </summary>
+		public DateTime SubmitTime { get; set; }
+		/// <summary>
+		/// 订单状态
+		/// </summary>
+		public int State { get; set; }
+	}
+}
